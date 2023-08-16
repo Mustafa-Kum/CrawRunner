e5e50c39b56dc8b5b7a17a147fd374e766095f92
Mustafa Kum <kum.mustafa96@gmail.com>
2023-08-16 19:16:13 +0300

    ReUpload

---
+++ Scripts/AudioManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    [SerializeField] private AudioSource[] sfx;
+    [SerializeField] private AudioSource[] bgm;
+
+    private int bgmIndex;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        if (!bgm[bgmIndex].isPlaying)
+            PlayRandomBGM();
+    }
+
+    public void PlayRandomBGM()
+    {
+        bgmIndex = Random.Range(0, bgm.Length);
+
+        PlayBGM(bgmIndex);
+    }
+
+    public void PlaySFX(int index)
+    {
+        if (index < sfx.Length)
+        {
+            sfx[index].pitch = Random.Range(0.85f, 1.15f);
+            sfx[index].Play();
+        }
+    }
+
+    public void StopSFX(int index)
+    {
+        sfx[index].Stop();
+    }
+
+    public void PlayBGM(int index)
+    {
+        for (int i = 0; i < bgm.Length; i++)
+        {
+            bgm[index].Stop();
+        }
+
+        bgm[index].Play();
+    }
+
+    public void StopBGM()
+    {
+        for (int i = 0; i < bgm.Length; i++)
+        {
+            bgm[i].Stop();
+        }
+    }
+}
---
+++ Scripts/UI_Main.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Main : MonoBehaviour
+{
+    private bool gamePaused;
+    private bool gameMuted;
+
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject endGame;
+
+    [Space]
+    [SerializeField] private TextMeshProUGUI lastScoreText;
+    [SerializeField] private TextMeshProUGUI highScoreText;
+    [SerializeField] private TextMeshProUGUI coinsText;
+
+    [Header("Volume Info")]
+    [SerializeField] private UI_VolumeSlider[] slider;
+    [SerializeField] private Image muteIcon;
+    [SerializeField] private Image inGameMuteIcon;
+
+
+    private void Start()
+    {
+        for (int i = 0; i < slider.Length; i++)
+        {
+            slider[i].SetupSlider();
+        }
+
+        SwitchMenuTo(mainMenu);
+
+        lastScoreText.text = "Last Score : " + PlayerPrefs.GetFloat("LastScore").ToString("#,#");
+        highScoreText.text = "High Score : " + PlayerPrefs.GetFloat("HighScore").ToString("#,#");
+    }
+
+    public void SwitchMenuTo(GameObject uiMenu)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+
+        uiMenu.SetActive(true);
+
+        AudioManager.instance.PlaySFX(8);
+
+        coinsText.text = PlayerPrefs.GetInt("Coins").ToString("#,#");
+    }
+
+    public void SwitchSkyBox(int index)
+    {
+        AudioManager.instance.PlaySFX(8);
+
+        GameManager.instance.SetupSkyBox(index);
+    }
+
+    public void MuteButton()
+    {    
+        gameMuted = !gameMuted;
+
+        if (gameMuted)
+        {
+            muteIcon.color = new Color(1, 1, 1, .5f);
+
+            AudioListener.volume = 0;
+        }
+        else
+        {
+            muteIcon.color = Color.white;
+            
+            AudioListener.volume = 1;
+        }
+    }
+
+    public void StartGameButton() 
+    {
+        muteIcon = inGameMuteIcon;
+
+        if (gameMuted)
+            muteIcon.color = new Color(1, 1, 1, .5f);
+
+        GameManager.instance.UnlockPlayer();
+    } 
+
+    public void PauseGameButton()
+    {
+        if (gamePaused)
+        {
+            Time.timeScale = 1;
+            gamePaused = false;
+        }
+        else
+        {
+            Time.timeScale = 0;
+            gamePaused = true;
+        }
+    }
+
+    public void RestartGameButton() => GameManager.instance.RestartLevel();
+
+    public void OpenEndGameUI()
+    {
+        SwitchMenuTo(endGame);
+    }
+}
